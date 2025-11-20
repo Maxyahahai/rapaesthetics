20feb92a6d450ddd8befae5d0469ca2ea45e6d14
Maxyahahai <maxchoudhary9910@gmail.com>
2025-11-20 22:13:18 +0530

    add vercel config

--- backend/server.js
+++ backend/server.js
@@ -149,3 +149,4 @@ app.use("/api/stats", wallpapersRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+module.exports = app;
